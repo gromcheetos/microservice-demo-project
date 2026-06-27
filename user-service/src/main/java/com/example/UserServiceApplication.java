33e6ea6661ac8b43e0043d2c3f3bb945a0918887
mariaprang <mprangishvili@newwork.com>
2026-06-27 13:32:24 +0400

    refactored database source and added migration files

--- user-service/src/main/java/com/example/UserServiceApplication.java
+++ user-service/src/main/java/com/example/UserServiceApplication.java
@@ -2,14 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
--- user-service/src/main/java/com/example/keycloak/KeycloakInitializer.java
+++ user-service/src/main/java/com/example/keycloak/KeycloakInitializer.java
@@ -20,7 +20,6 @@ import java.util.List;
 
 @Slf4j
 @Component
-
 public class KeycloakInitializer implements ApplicationRunner {
 
     private final Keycloak masterClient;
